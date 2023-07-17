09351f08dcb23eaa825ea16d3c97390159148aa0
Garrett Crabb <garrettac01@gmail.com>
2023-07-17 15:21:26 -0500

    added creating a new tag feature

--- Tabloid/client/src/components/TagList.js
+++ Tabloid/client/src/components/TagList.js
@@ -1,19 +1,29 @@
 import React, {useEffect, useState} from "react";
 import Tag from "./Tag";
 import { getAllTags } from "../modules/tagManager";
+import { useNavigate } from "react-router-dom";
 
 export default function TagList() {
     const [tags, setTags] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         getAllTags().then(setTags);
     }, []);
+
+    const handleChange = (evt) => {
+        evt.preventDefault();
+        navigate("/Tag/add")
+    }
 
     return (
         <section>
             {tags.map((t) => (
                 <Tag key={t.id} tag={t} />
             ))}
+            <form>
+                <button onClick={handleChange}>Create Tag</button>
+            </form>
         </section>
     )
 }
