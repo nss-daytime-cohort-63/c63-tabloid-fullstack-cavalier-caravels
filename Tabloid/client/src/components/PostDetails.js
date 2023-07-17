2133d5ae411bf5d310197d993d5c52c575c1b237
Lincoln Keesecker <lekeesecker@gmail.com>
2023-07-17 14:32:21 -0500

    can create categories

--- Tabloid/client/src/components/PostDetails.js
+++ Tabloid/client/src/components/PostDetails.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getPost } from "../modules/postManager";
+import { getPost } from "../modules/postManager.js";
 
 
 const PostDetails = () => {
