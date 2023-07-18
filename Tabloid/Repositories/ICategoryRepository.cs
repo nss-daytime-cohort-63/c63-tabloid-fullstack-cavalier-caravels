cf1bd4f1c778084312153e93172c6b9cad5792ce
Garrett Crabb <garrettac01@gmail.com>
2023-07-18 14:29:14 -0500

    added tag name edit feature

--- Tabloid/Repositories/ICategoryRepository.cs
+++ Tabloid/Repositories/ICategoryRepository.cs
@@ -9,7 +9,6 @@ namespace Tabloid.Repositories
     void Add(Category category);
     void Delete(int id);
     List<Category> GetAll();
-    Category GetById(int id);
     void Update(Category category);
     }
 }
